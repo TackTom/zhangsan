2512e2cbe7f3b3f86f1bf7210caecbdb28146511
LinChuanjiang <lin1161957303@163.com>
2021-09-09 12:46:15 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,42 @@
+"""
+print("hello world!")#字符串
+print(2333)#整数
+print(2.333)#小数
+print(True)#布尔值  True False
+print(())#元组
+print([])#数组
+print({})#字典
+
+
+多行注释三个引号
+锄禾日当午
+
+print("haha",2333,2.3333)
+print("哈哈"+"嘻嘻")#字符串的拼接
+print("哈哈"*10)
+print(((1+2)*100-9.9)/2)
+print(2>3)
+print(2<3)
+
+#变量
+#赋值
+name ="张三" #把张三这个值赋值给了名字叫name的这个变量
+print(name)
+"""
+# a = input("请输入：")
+# b = input("请输入：")
+# print("input获取的内容：",a+b)
+
+#数据格式的转换
+
+# print(type("哈哈"))
+# print(type(300))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+a = int(input("请输入："))
+b = int(input("请输入："))
+print("input获取的内容：",a+b)
